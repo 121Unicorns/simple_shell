e6a46f6c5eefd73be466c69ffc58ec7c595bc7b9
Mich <mich@unicorn>
2022-12-23 22:52:30 +0300

    Commit Solutions

--- prompt_user.c
+++ prompt_user.c
@@ -1,5 +1,4 @@
 #include "shell.h"
-
 /**
 * prompt_user - prints $ to let user know the program is
 * ready to take their input
@@ -8,8 +7,8 @@
 */
 void prompt_user(void)
 {
-	if ((isatty(STDIN_FILENO) == 1) && (isatty(STDOUT_FILENO) == 1))
-		flags.interactive = 1;
-	if (flags.interactive)
-		write(STDERR_FILENO, "$ ", 2);
+if ((isatty(STDIN_FILENO) == 1) && (isatty(STDOUT_FILENO) == 1))
+flags.interactive = 1;
+if (flags.interactive)
+write(STDERR_FILENO, "$ ", 2);
 }
